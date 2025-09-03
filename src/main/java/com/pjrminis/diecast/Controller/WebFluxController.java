b444064dfdfd6feccdecd865411f1e8c885b309b
paulo.winicius.jr <paulo.winicius.jr@gmail.com>
2025-09-03 17:07:52 -0300

    Adicionando camada de webflux

---
+++ src/main/java/com/pjrminis/diecast/Controller/WebFluxController.java
@@ -0,0 +1,16 @@
+package com.pjrminis.diecast.Controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api")
+@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+public class WebFluxController {
+
+    @GetMapping("/webflux")
+    public Mono<String> helloWebFlux() {
+        return Mono.just("Olá, WebFlux!");
+    }
+}
